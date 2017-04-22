d28dafa895753a00fcb97fa47be8c069d0d716f8
dcarrsf <dan@dancarr.co>
2017-04-22 12:40:12 -0700

    Clean up for linting.

--- src/decorators/Query.js
+++ src/decorators/Query.js
@@ -2,7 +2,7 @@ import Utils from '../singleton/Utils.js';
 import Result from './Result.js';
 
 // Singleton
-const _utils = Utils.shared();
+const utils = Utils.shared();
 
 // API: Query
 export default class Query {
@@ -10,7 +10,7 @@ export default class Query {
     // Selector as String
     this.selector = selector;
     // Decorate NodeList
-    this.nodeList = _utils.queryAll(selector);
+    this.nodeList = utils.queryAll(selector);
     // Wrap nodes
     this.results = this.map((item) => new Result(item));
   }
@@ -19,7 +19,7 @@ export default class Query {
 
   map(func) {
     const newArr = [];
-    // Build our own 'map' because 
+    // Build our own 'map' because
     // nodeList is not an array.
     this.nodeList.forEach((item) => {
       newArr.push(func.call(null, item));
